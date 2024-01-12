eb6aea69969844d085a8809179bfbc9757e8003f
Sieglinde <sieglindevwb@gmail.com>
2024-01-12 14:50:05 +0100

    Delete unneeded var_dumps

--- index.php
+++ index.php
@@ -8,9 +8,6 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-var_dump($_GET);
-var_dump($_POST);
-
 // Load your classes
 require_once 'classes/Data.php';
 require_once 'classes/LanguageGame.php';
